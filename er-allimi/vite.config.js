f129a9ac3e33a4af738fa32c7ce9970a4b5acd27
hyejin <yhj970102@gmail.com>
2023-08-12 15:41:45 +0900

    [Chore] alias 설정

--- er-allimi/vite.config.js
+++ er-allimi/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve('', './src'),
+      '@assets': path.resolve('', './src/assets'),
+      '@components': path.resolve('', './src/components'),
+      '@constants': path.resolve('', './src/constants'),
+      '@hooks': path.resolve('', './src/hooks'),
+      '@pages': path.resolve('', './src/pages'),
+      '@services': path.resolve('', './src/services'),
+      '@stores': path.resolve('', './src/stores'),
+      '@utils': path.resolve('', './src/utils'),
+    }
+  },
 })
