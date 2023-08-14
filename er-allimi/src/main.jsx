1f5f0f6f7ba227e676c545818b5977bae5b46402
ming0830 <minsun0830@naver.com>
2023-08-14 14:31:49 +0900

    [Style] 기본css import 삭제

--- er-allimi/src/main.jsx
+++ er-allimi/src/main.jsx
@@ -4,7 +4,6 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { router } from './routers';
-// import './index.css'
 
 const queryClient = new QueryClient({
 	defaultOptions : {
