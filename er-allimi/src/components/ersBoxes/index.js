9f0d510b0eb826c874f7e87e108db0f3368c8566
hyejin <yhj970102@gmail.com>
2023-08-15 17:05:03 +0900

    [Feat] ErsBox 컴포넌트 구현

--- er-allimi/src/components/ersBoxes/index.js
+++ er-allimi/src/components/ersBoxes/index.js
@@ -1,2 +1,5 @@
 export { default as CurrentLocationInput } from './CurrentLocationInput';
 export { default as CurrentLocationBox } from './CurrentLocationBox';
+export { default as ErsBox } from './ErsBox';
+export { default as ErsList } from './ErsList';
+export { default as ErItem } from './ErItem';
--- er-allimi/src/components/index.js
+++ er-allimi/src/components/index.js
@@ -2,6 +2,9 @@ export { Navbar } from './app';
 
 export { CurrentLocationInput } from './ersBoxes';
 export { CurrentLocationBox } from './ersBoxes';
+export { ErsBox } from './ersBoxes';
+export { ErsList } from './ersBoxes';
+export { ErItem } from './ersBoxes';
 
 export { Box } from './ui';
 export { Input } from './ui';
@@ -12,3 +15,4 @@ export { FaMapMarkedAlt } from './icons';
 export { AiOutlineInfoCircle } from './icons';
 export { RxHamburgerMenu } from './icons';
 export { IoLocationSharp } from './icons';
+export { BiSolidPhone } from './icons';
