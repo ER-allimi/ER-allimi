78ae98695f28ad311eadc682418ff6dc53a83a4b
hyejin <yhj970102@gmail.com>
2023-08-16 00:16:21 +0900

    [Feat] Pagination 컴포넌트 구현

--- er-allimi/src/components/ui/index.js
+++ er-allimi/src/components/ui/index.js
@@ -2,3 +2,4 @@ export { default as Box } from './Box';
 export { default as Input } from './Input';
 export { default as Button } from './Button';
 export { default as Dropdown } from './Dropdown';
+export { default as Pagination } from './Pagination';
