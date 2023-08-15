621155503b3754136e32b418254cfde165b39656
hyejin <yhj970102@gmail.com>
2023-08-15 22:14:08 +0900

    [Feat] ErsBox에 RadiusDropdown 추가 및 스타일 이동

--- er-allimi/src/components/ersBoxes/ErsBox.jsx
+++ er-allimi/src/components/ersBoxes/ErsBox.jsx
@@ -1,39 +1,50 @@
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import { Box, Button, ErsList } from '@components';
+import { Box, Button, ErsList, RadiusDropdown } from '@components';
 
 function ErsBox({ className }) {
+  const StyledErsBox = styled(Box)`
+    max-height: calc(100vh - 200px);
+  `;
+
+  const Title = styled.h2`
+    margin-bottom: 0.5rem;
+  `;
+
+  const Utils = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 0.5rem;
+  `;
+
+  const SortingButtons = styled.div`
+    display: flex;
+    align-items: center;
+
+    button {
+      margin-right: 0.5rem;
+    }
+  `;
+
   return (
     <StyledErsBox className={className}>
       <Title>내 위치 주변 응급실</Title>
-      <SortingButtons>
-        <Button color="gray" round="lg">
-          거리순
-        </Button>
-        <Button color="gray" round="lg" outline>
-          가용 병상 개수 순
-        </Button>
-      </SortingButtons>
+      <Utils>
+        <SortingButtons>
+          <Button color="gray" round="lg">
+            거리순
+          </Button>
+          <Button color="gray" round="lg" outline>
+            가용 병상 개수 순
+          </Button>
+        </SortingButtons>
+        <RadiusDropdown />
+      </Utils>
       <ErsList />
     </StyledErsBox>
   );
 }
-
-const StyledErsBox = styled(Box)`
-  max-height: calc(100vh - 200px);
-`;
-
-const Title = styled.h2`
-  margin-bottom: 0.5rem;
-`;
-
-const SortingButtons = styled.div`
-  margin-bottom: 0.5rem;
-
-  button {
-    margin-right: 0.5rem;
-  }
-`;
 
 ErsBox.propTypes = {
   className: PropTypes.string,
