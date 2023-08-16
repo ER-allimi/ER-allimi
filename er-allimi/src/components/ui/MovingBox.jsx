81ccd70688eae6d96795eadc90feecb17eb1414e
hyejin <yhj970102@gmail.com>
2023-08-17 01:37:25 +0900

    [Feat] MovingBox에 children prop 추가 및 스타일 변경

--- er-allimi/src/components/ui/MovingBox.jsx
+++ er-allimi/src/components/ui/MovingBox.jsx
@@ -4,8 +4,8 @@ import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import { VscTriangleDown, VscTriangleUp } from '@components';
 
-function MovingBox({ className }) {
-  const [isExpanded, setIsExpanded] = useState(false);
+function MovingBox({ className, children }) {
+  const [isExpanded, setIsExpanded] = useState(true);
 
   const handleTriangleClick = () => {
     setIsExpanded(!isExpanded);
@@ -13,10 +13,10 @@ function MovingBox({ className }) {
 
   return (
     <StyledMovingBox className={className} isExpanded={isExpanded}>
-      <div onClick={handleTriangleClick}>
+      <ExpandIcon onClick={handleTriangleClick}>
         {isExpanded ? <VscTriangleDown /> : <VscTriangleUp />}
-      </div>
-      무빙박스
+      </ExpandIcon>
+      {children}
     </StyledMovingBox>
   );
 }
@@ -30,18 +30,20 @@ const expand = ({ isExpanded }) =>
 const StyledMovingBox = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: start;
   align-items: center;
-  justify-content: space-between;
-  padding: 0 1rem 0.7rem 1rem;
+  padding: 0rem 1rem 0.7rem 1rem;
   width: 100%;
   height: 100%;
   background-color: white;
-  border-radius: 0.5rem 0.5rem 0 0;
+  border-radius: 0.7rem 0.7rem 0 0;
   box-shadow: -3px 0 5px 3px ${({ theme }) => theme.colors.grayLight};
   transition: transform 0.3s linear;
 
   ${expand}
+`;
 
+const ExpandIcon = styled.div`
   svg {
     font-size: 2rem;
     color: ${({ theme }) => theme.colors.gray};
@@ -51,6 +53,7 @@ const StyledMovingBox = styled.div`
 
 MovingBox.propTypes = {
   className: PropTypes.string,
+  children: PropTypes.node,
 };
 
 export default MovingBox;
