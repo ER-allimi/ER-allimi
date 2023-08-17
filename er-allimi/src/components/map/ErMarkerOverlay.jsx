b982965530351eb5ca488a9c32a0f1a29bdee281
ming0830 <minsun0830@naver.com>
2023-08-17 21:15:07 +0900

    [Feat] 호버시 인포윈도우 구현

--- er-allimi/src/components/map/ErMarkerOverlay.jsx
+++ er-allimi/src/components/map/ErMarkerOverlay.jsx
@@ -1,18 +1,39 @@
-import { FaMapMarker } from '@components/icons';
+import { FaMapMarker, BsFillCircleFill } from '@components';
 import PropTypes from 'prop-types';
-import styles from '../../styles/mapOverlay/erMarkerOverlay.module.css'
+import styles from '../../styles/mapOverlay/erMarkerOverlay.module.css';
 
 ErMarkerOverlay.propTypes = {
+  name: PropTypes.string.isRequired,
+  availableBed: PropTypes.number,
+  totalBed: PropTypes.number,
   markerColor: PropTypes.string.isRequired,
   order: PropTypes.number.isRequired,
 };
 
-/** 색상과 순번을 표현하는 마커 오버레이 */
-function ErMarkerOverlay({ markerColor, order }) {
+/** 색상과 순번을 표현하는 마커와 호버시 기본정보를 나타내는 오버레이 */
+function ErMarkerOverlay({ name, availableBed, totalBed, markerColor, order }) {
   return (
-    <div className={styles.markerContainer}>
-      <FaMapMarker size={38} color={markerColor} />
-      <p className={styles.markerText}>{order}</p>
+    <div className={styles.container}>
+      <div className={styles.markerContainer}>
+        <FaMapMarker size={38} color={markerColor} />
+        <p className={styles.markerText}>{order}</p>
+        <div className={styles.infoWindowContainer}>
+          <div className={styles.infoWindowContent}>
+            <h5 className={styles.hpName}>{name}</h5>
+            {availableBed && totalBed && (
+              <div className={styles.bedData}>
+                <BsFillCircleFill size={15} color={markerColor} />
+                <div className={styles.bedDataText}>
+                  가용 병상 수&nbsp;&nbsp;
+                  <span className={styles.num}>{availableBed}</span>
+                  &nbsp;/&nbsp;전체&nbsp;
+                  <span className={styles.num}>{totalBed}</span>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
