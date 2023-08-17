fe4ab3b086f3d415e114c096101c26b4d899428f
hyejin <yhj970102@gmail.com>
2023-08-17 14:49:36 +0900

    [Comment] router에서 chartView path 주석처리

--- er-allimi/src/routers.jsx
+++ er-allimi/src/routers.jsx
@@ -1,42 +1,33 @@
-import { createBrowserRouter } from "react-router-dom";
-import {
-    PATH_ROOT,
-    PATH_CHARTVIEW,
-    PATH_HOSPITALDETAIL,
-} from '@constants';
+import { createBrowserRouter } from 'react-router-dom';
+import { PATH_ROOT, PATH_CHARTVIEW, PATH_HOSPITALDETAIL } from '@constants';
 import App from './App';
-import { 
-    MapView, 
-    ChartView,
-    HpDetailPage,
-    Chart 
-} from '@pages';
+import { MapView, ChartView, HpDetailPage, Chart } from '@pages';
 
 const router = createBrowserRouter([
-    {
-        path: PATH_ROOT,
-        element: <App />,
-        children: [
-            {
-                index: true,
-                element: <MapView />,    // MapView > 지도 + ersBoxes
-            },
-            {
-                path: PATH_HOSPITALDETAIL,
-                element: <HpDetailPage />,    // HpDetailPage > 지도 + HpDetailBoxes
-            },
-            {
-                path: PATH_CHARTVIEW,
-                element: <ChartView />,  // ChartView > chart + ersBoxes
-                children: [
-                    {
-                        index: true,
-                        element: <Chart />,   
-                    },
-                ]
-            },
-        ]
-    }
-])
+  {
+    path: PATH_ROOT,
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <MapView />, // MapView > 지도 + ersBoxes
+      },
+      {
+        path: PATH_HOSPITALDETAIL,
+        element: <HpDetailPage />, // HpDetailPage > 지도 + HpDetailBoxes
+      },
+      // {
+      //     path: PATH_CHARTVIEW,
+      //     element: <ChartView />,  // ChartView > chart + ersBoxes
+      //     children: [
+      //         {
+      //             index: true,
+      //             element: <Chart />,
+      //         },
+      //     ]
+      // },
+    ],
+  },
+]);
 
 export { router };
