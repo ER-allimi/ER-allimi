806aa16ce7349200821806938808abe8af584913
hyejin <yhj970102@gmail.com>
2023-08-19 21:09:44 +0900

    [Feat] 기본 위치 상수화 및 userLocationState 값 수정

--- er-allimi/src/stores/atoms/userLocationState.js
+++ er-allimi/src/stores/atoms/userLocationState.js
@@ -1,14 +1,20 @@
 import { atom } from 'recoil';
-// 위치 기본값 서울역
+import {
+  DEFAULT_LATITUDE,
+  DEFAULT_LONGITUDE,
+  DEFAULT_ADDRESS,
+  DEFAULT_ROAD_ADDRESS,
+} from '@constants';
 
+// 위치 기본값 서울역
 const userLocationState = atom({
   key: 'userLocationState',
   default: {
-    latitude: 37.55460495198728,
-    longitude: 126.97058657543889,
+    latitude: DEFAULT_LATITUDE,
+    longitude: DEFAULT_LONGITUDE,
     address: {
-      address: '동자동 43-205',
-      road_address: '서울 용산구 한강대로 405',
+      address: DEFAULT_ADDRESS,
+      road_address: DEFAULT_ROAD_ADDRESS,
     },
   },
 });
