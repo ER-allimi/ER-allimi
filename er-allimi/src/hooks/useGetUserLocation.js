db8f3399477db01b51868e1ee0358c20d1cb9723
hyejin <yhj970102@gmail.com>
2023-08-19 21:17:07 +0900

    [Feat] useGetUserLocation hook 수정

--- er-allimi/src/hooks/useGetUserLocation.js
+++ er-allimi/src/hooks/useGetUserLocation.js
@@ -1,10 +1,11 @@
 import { useSetRecoilState } from 'recoil';
-import { userLocationState } from '@stores';
+import { userLocationState, mapCenterPointState } from '@stores';
 import { getAddressByCoor } from '@utils';
 
 // 사용자의 위치 정보를 가져와 recoil에 저장
 function useGetUserLocation() {
-  const setUserLocationState = useSetRecoilState(userLocationState);
+  const setUserLocation = useSetRecoilState(userLocationState);
+  const setMapCenterPoint = useSetRecoilState(mapCenterPointState);
 
   const handleGetCurPosSuccess = async (position) => {
     const latitude = position.coords.latitude;
@@ -13,7 +14,7 @@ function useGetUserLocation() {
     const addressData = await getAddressByCoor({ latitude, longitude });
     const { address, road_address } = addressData;
 
-    setUserLocationState({
+    setUserLocation({
       latitude,
       longitude,
       address: {
@@ -21,10 +22,17 @@ function useGetUserLocation() {
         road_address: road_address?.address_name,
       },
     });
+
+    setMapCenterPoint({
+      latitude,
+      longitude,
+    });
   };
 
   const handleGetCurPosFail = (err) => {
-    console.log(`사용자의 위치 정보를 가져오지 못했습니다.(${err.message})`);
+    alert(
+      `사용자의 위치 정보를 가져오지 못했습니다.\n'응급실알리미'에서 위치 정보를 가져올 수 있게, \n설정에서 위치 접근을 허용해주세요.`,
+    );
   };
 
   return { handleGetCurPosSuccess, handleGetCurPosFail };
