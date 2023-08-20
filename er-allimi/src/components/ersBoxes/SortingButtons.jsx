3e00b51a8ecfacc43b6d2d0fe1f40fcb1b43d024
hyejin <yhj970102@gmail.com>
2023-08-20 17:02:55 +0900

    [Feat] ErsContent에서 정렬 버튼을 떼어내 따로 SortingButtons생성

---
+++ er-allimi/src/components/ersBoxes/SortingButtons.jsx
@@ -0,0 +1,54 @@
+import { useRecoilState } from 'recoil';
+import styled from '@emotion/styled';
+import { Button } from '@components';
+import { sortingState } from '@stores';
+import { SORTING_DISTANCE, SORTING_AVAILABLE_BED } from '@constants';
+
+function SortingButtons() {
+  const [sorting, serSorting] = useRecoilState(sortingState);
+
+  const handleButtonClick = (sortBy) => {
+    serSorting(sortBy);
+  };
+
+  const StyledSortingButtons = styled.div`
+    display: flex;
+    align-items: center;
+
+    button {
+      margin-right: 0.5rem;
+      padding: 0.2rem 0.6rem;
+
+      @media (max-width: ${({ theme }) => theme.breakPoints.md}) {
+        font-size: 12px;
+      }
+
+      @media (max-width: ${({ theme }) => theme.breakPoints.sm}) {
+        font-size: 11px;
+      }
+    }
+  `;
+
+  return (
+    <StyledSortingButtons>
+      <Button
+        color="gray"
+        round="lg"
+        outline={sorting !== SORTING_DISTANCE}
+        onClick={() => handleButtonClick(SORTING_DISTANCE)}
+      >
+        거리순
+      </Button>
+      <Button
+        color="gray"
+        round="lg"
+        outline={sorting !== SORTING_AVAILABLE_BED}
+        onClick={() => handleButtonClick(SORTING_AVAILABLE_BED)}
+      >
+        가용 병상 개수 순
+      </Button>
+    </StyledSortingButtons>
+  );
+}
+
+export default SortingButtons;
