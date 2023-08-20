dbdbb968e9a8e5c12a8372ddd96f65b349ec433f
ming0830 <minsun0830@naver.com>
2023-08-21 01:07:22 +0900

    [Feat] 여닫기가 가능한 박스 ui 구현

--- er-allimi/src/components/ui/index.js
+++ er-allimi/src/components/ui/index.js
@@ -5,3 +5,4 @@ export { default as Dropdown } from './Dropdown';
 export { default as Pagination } from './Pagination';
 export { default as Toggle } from './Toggle';
 export { default as MovingBox } from './MovingBox';
+export { default as ClosedBox } from './ClosedBox';
