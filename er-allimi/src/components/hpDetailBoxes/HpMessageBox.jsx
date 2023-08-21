3f9a20fbce18c6c45199f94a2674686f1ccbd488
ming0830 <minsun0830@naver.com>
2023-08-21 18:38:04 +0900

    [Feat] 메시지 박스 컴포넌트 react-query 훅 호출

--- er-allimi/src/components/hpDetailBoxes/HpMessageBox.jsx
+++ er-allimi/src/components/hpDetailBoxes/HpMessageBox.jsx
@@ -1,8 +1,18 @@
 import PropTypes from 'prop-types';
 import { Box } from '@components';
+import { useFetchHpMsg } from '@hooks';
+import { useRecoilValue } from 'recoil';
+import { targetHpIdState } from '@stores';
 
 function HpMessageBox({ className }) {
-  return <Box className={className}>응급실 메시지</Box>;
+  const targetHpId = useRecoilValue(targetHpIdState);
+  const { data, isLoading, isError } = useFetchHpMsg(targetHpId);
+
+  return (
+    <>
+      <Box className={className}>응급실 메시지</Box>;
+    </>
+  );
 }
 
 HpMessageBox.propTypes = {
