db0d410f21740e0b1bc0d0ab7429e09a54f38147
ming0830 <minsun0830@naver.com>
2023-08-23 07:25:50 +0900

    [Refactor] 마커 훅 생성으로 마커 생성 부분 제거

--- er-allimi/src/pages/Map.jsx
+++ er-allimi/src/pages/Map.jsx
@@ -5,30 +5,25 @@ import {
   ZoomInButton,
   ZoomOutButton,
   CurrentLocationOverlay,
-  ErMarkerOverlay,
-  InfoWindowOverlay,
 } from '@components';
-import { useNavigate } from 'react-router-dom';
-import { getErRTavailableBedByColor, getPathHospitalDetail } from '@utils';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getPathHospitalDetail } from '@utils';
 import { renderToString } from 'react-dom/server';
 import { useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
 import {
   userLocationState,
   mapCenterPointState,
-  sortedErsWithRadiusState,
   radiusState,
   ersPaginationState,
 } from '@stores';
-import { KM_TO_M_UNIT, ERS_CNT_PER_PAGE } from '@constants';
+import { KM_TO_M_UNIT } from '@constants';
+import { useMarker } from '@hooks';
 
 const { kakao } = window;
-const DEFAULT_MARKER_COLOR = '#222222';
 
 function Map() {
   const { latitude, longitude } = useRecoilValue(userLocationState);
   const setCenterPoint = useSetRecoilState(mapCenterPointState);
-  const sortedErsWithRadius = useRecoilValue(sortedErsWithRadiusState);
-  const ersPagination = useRecoilValue(ersPaginationState);
   const resetPagination = useResetRecoilState(ersPaginationState);
 
   // 지도 표시될 HTML 요소
@@ -39,8 +34,10 @@ function Map() {
   const [locPosition, setLocPosition] = useState(defaultCenter);
   const [centerPosition, setCenterPosition] = useState(defaultCenter);
   const [circleOverlay, setCircleOverlay] = useState(null);
-  const [erMarkers, setErMarkers] = useState([]);
+  const erMarkers = useMarker(map, setupMarkerEventListeners)
   const navigate = useNavigate();
+  const hpIdParameter = useParams().hospitalId
+
 
   /** 카카오 지도 생성 */
   const createMap = () => {
@@ -64,66 +61,9 @@ function Map() {
     setCenterPosition(newLocPosition);
     setCenterPoint({ latitude, longitude });
   };
-
-  /** 현재 위치에서 반경 내에 있는 응급실 마커 그려줌 */
-  const createNearByErMarker = () => {
-    //모든 마커 지우기
-    erMarkers.forEach((marker) => marker.setMap(null));
-    setErMarkers([]);
-
-    const start = (ersPagination - 1) * ERS_CNT_PER_PAGE;
-    const end = ersPagination * ERS_CNT_PER_PAGE;
-    const sortedErsPerPage = sortedErsWithRadius.slice(start, end);
-
-    const nearByErCount = sortedErsPerPage.length;
-    const newErMarkers = sortedErsPerPage.map((item, idx) => {
-      const hpInfo = item.hpInfo;
-      const availableBedInfo = item.availableBedInfo;
-      const name = hpInfo.dutyName;
-      const lat = hpInfo.wgs84Lat;
-      const lon = hpInfo.wgs84Lon;
-
-      const availableBed = availableBedInfo ? availableBedInfo.hvec : null;
-      const totalBed = availableBedInfo ? availableBedInfo.hvs01 : null;
-
-      const markerColor = totalBed
-        ? getErRTavailableBedByColor(availableBed, totalBed)
-        : DEFAULT_MARKER_COLOR;
-
-      const styledMarker = renderToString(
-        <ErMarkerOverlay markerColor={markerColor} order={start + idx + 1} />,
-      );
-      const styledInfoWindow = renderToString(
-        <InfoWindowOverlay
-          name={name}
-          availableBed={availableBed}
-          totalBed={totalBed}
-          markerColor={markerColor}
-        />,
-      );
-
-      const newMarker = new kakao.maps.CustomOverlay({
-        position: new kakao.maps.LatLng(lat, lon),
-        map: map,
-        content: styledMarker,
-        clickable: true,
-        zIndex: nearByErCount - idx + 1,
-      });
-
-      const newInfoWindow = new kakao.maps.CustomOverlay({
-        content: styledInfoWindow,
-        position: newMarker.getPosition(),
-        yAnchor: 1,
-        zIndex: nearByErCount + 1,
-      });
-      setupMarkerEventListeners(hpInfo.hpid, newInfoWindow);
-      return newMarker;
-    });
-    setErMarkers(newErMarkers);
-  };
-
+  
   /** 마커의 이벤트 리스너 설정 */
-  const setupMarkerEventListeners = (hospitalId, infoWindow) => {
+  function setupMarkerEventListeners(hospitalId, infoWindow)  {
     const markerDiv = document.querySelectorAll('.markerHover');
     const lastElement = markerDiv[markerDiv.length - 1];
 
@@ -165,28 +105,22 @@ function Map() {
   // 첫 렌더링 시 지도 생성
   useEffect(() => {
     createMap();
+    
   }, [latitude, longitude]);
-
+  ``;
   // 중심 위치 변경 시 응급실 마커, 반경 오버레이 생성
   useEffect(() => {
     if (!map) return;
+
     kakao.maps.event.addListener(map, 'center_changed', handleCenterChange);
-    createNearByErMarker();
+    erMarkers.map(marker => marker.setMap(map))
 
     // 기존 circle 오버레이 제거
     circleOverlay && circleOverlay.setMap(null);
-
     newCircleOverlay.setMap(map);
-
     setCircleOverlay(newCircleOverlay);
-  }, [map, centerPosition, radius, ersPagination]);
 
-  // useEffect(() => {
-  //   if (!map) return;
-  //   erMarkers.forEach((marker) => marker.setMap(null))
-  //   createNearByErMarker();
-
-  // }, [map, sortedErsWithRadius, ersPagination]);
+  }, [map, centerPosition, radius, erMarkers]);
 
   return (
     <MapContainer ref={mapContainer}>
