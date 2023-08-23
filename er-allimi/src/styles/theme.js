56df679b62a85a5342bdf84376cf537e752141c5
hyejin <yhj970102@gmail.com>
2023-08-23 17:54:35 +0900

    [Feat] theme에 blue 색 추가

--- er-allimi/src/styles/theme.js
+++ er-allimi/src/styles/theme.js
@@ -1,31 +1,36 @@
 const theme = {
-    colors: {
-        gray: 'rgba(52, 52, 52, 33%)',
-        grayDark: 'rgba(52, 52, 52, 81%)',
-        grayDarker: '#222222',
-        grayLight: 'rgba(163, 163, 163, 26.67%)',
-        grayLighter: 'rgba(245, 245, 245, 81%)',
-        red: '#E31926',
-        redDark: '#C41621',
-        redDarker: '#A6121C',
-        redLight: '#EA454F',
-        redLighter: '#F0727B',
-        yellow: '#FFE710',
-        yellowDark: '#EDD500',
-        yellowDarker: '#CBB700',
-        yellowLight: '#FFEA32',
-        yellowLighter: '#FFEE54',
-        green: '#2B8F06',
-        greenDark: '#216E05',
-        greenDarker: '#174E03',
-        greenLight: '#35B007',
-        greenLighter: '#3FD009',
-    },
-    breakPoints: {
-        sm: '320px',   // mobile
-        md: '760px',   // tablet
-        lg: '1024px',  // laptop
-    },
-}
+  colors: {
+    gray: 'rgba(52, 52, 52, 33%)',
+    grayDark: 'rgba(52, 52, 52, 81%)',
+    grayDarker: '#222222',
+    grayLight: 'rgba(163, 163, 163, 26.67%)',
+    grayLighter: 'rgba(245, 245, 245, 81%)',
+    red: '#E31926',
+    redDark: '#C41621',
+    redDarker: '#A6121C',
+    redLight: '#EA454F',
+    redLighter: '#F0727B',
+    yellow: '#FFE710',
+    yellowDark: '#EDD500',
+    yellowDarker: '#CBB700',
+    yellowLight: '#FFEA32',
+    yellowLighter: '#FFEE54',
+    green: '#2B8F06',
+    greenDark: '#216E05',
+    greenDarker: '#174E03',
+    greenLight: '#35B007',
+    greenLighter: '#3FD009',
+    blue: '#0077a9',
+    blueDark: '#005f87',
+    blueDarker: '#004765',
+    blueLight: '#008fcb',
+    blueLighter: '#00a7ed',
+  },
+  breakPoints: {
+    sm: '320px', // mobile
+    md: '760px', // tablet
+    lg: '1024px', // laptop
+  },
+};
 
 export { theme };
