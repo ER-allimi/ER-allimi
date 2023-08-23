8c5b6b858d6b2ab81c6994380ccb3f8c918406d5
hyejin <yhj970102@gmail.com>
2023-08-23 17:55:16 +0900

    [Feat] classifySurgery 함수 생성

--- er-allimi/src/utils/index.js
+++ er-allimi/src/utils/index.js
@@ -4,3 +4,4 @@ export { getErRTavailableBedByColor } from './getErRTavailableBedByColor';
 export { getAddressByCoor } from './getAddressByCoor';
 export { getCoorByAddress } from './getCoorByAddress';
 export { copyText } from './copyText';
+export { classifySurgery } from './classifySurgery';
