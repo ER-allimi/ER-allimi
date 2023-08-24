4e22cc0a47d9ef51ece8f4d6f1f79690df1a9d56
ming0830 <minsun0830@naver.com>
2023-08-25 03:03:33 +0900

    [Feat] 게이지차트 컴포넌트

---
+++ er-allimi/src/components/hpDetailBoxes/ErChart.jsx
@@ -0,0 +1,110 @@
+import PropTypes from 'prop-types';
+import { getErRTavailableBedByColor } from '@utils';
+import styled from '@emotion/styled';
+import bb, { gauge } from 'billboard.js';
+import { useEffect, useRef } from 'react';
+
+function ErChart({ availableBed, totalBed, hpid }) {
+  const rate = Math.round((availableBed / totalBed) * 100);
+  const colorByrate = getErRTavailableBedByColor(availableBed, totalBed);
+  const chartRef = useRef(null);
+  useEffect(() => {
+    console.log(hpid, '후');
+    const chart = bb.generate({
+      data: {
+        columns: [['가용병상', 0]],
+        type: gauge(),
+      },
+      size: {
+        width: 60,
+        height: 100,
+      },
+      legend: {
+        item: {
+          tile: {
+            width: 5,
+            height: 5,
+          },
+        },
+      },
+      tooltip: {
+        format: {
+          value: function () {
+            return availableBed + '자리';
+          },
+        },
+      },
+      gauge: {
+        background: '#CCCCCC',
+        label: {
+          format: function (value) {
+            return value + '%';
+          },
+        },
+      },
+      color: {
+        pattern: [colorByrate],
+      },
+      bindto: chartRef.current,
+    });
+
+    setTimeout(function () {
+      chart.load({
+        columns: [['가용병상', rate]],
+      });
+    }, 500);
+    return () => {
+      chart.destroy();
+    };
+  }, [hpid]);
+
+  return (
+    <StyledChartContainer>
+      <div ref={chartRef} />
+    </StyledChartContainer>
+  );
+}
+const StyledChartContainer = styled.div`
+  .bb-legend-item text {
+    font-size: 5px;
+    font-weight: 500;
+  }
+  .bb-tooltip-container {
+    z-index: 1;
+  }
+  .bb-tooltip {
+    background-color: ${({ theme }) => theme.colors.grayDarker};
+    color: white;
+    border-radius: 0.5rem;
+    width: 100px;
+    box-shadow: 3px 3px 5px 3px rgba(163, 163, 163, 26.67%);
+    padding: 0.2rem;
+  }
+  .bb-chart-arcs-gauge-min,
+  .bb-chart-arcs-gauge-max {
+    font-size: 5px;
+  }
+  .bb-gauge-value {
+    font-size: 8.5px !important;
+    font-weight: 600;
+  }
+  .bb-tooltip-container .name,
+  .bb-tooltip-container .value {
+    font-size: 11px;
+    font-weight: 500;
+  }
+  .bb-tooltip-container .name {
+    top: 50px;
+  }
+  .bb-legend {
+    transform: translate(10px, 25px);
+  }
+`;
+
+ErChart.propTypes = {
+  availableBed: PropTypes.number,
+  totalBed: PropTypes.number,
+  hpid: PropTypes.string,
+};
+
+export default ErChart;
