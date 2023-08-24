b39a67a6b380708be1498114d86b44cc39e4e8f5
ming0830 <minsun0830@naver.com>
2023-08-25 04:42:19 +0900

    [Feat] 음압, 일반 격리일 경우 비율 분기점 생성

--- er-allimi/src/utils/getErRTavailableBedByColor.js
+++ er-allimi/src/utils/getErRTavailableBedByColor.js
@@ -1,17 +1,32 @@
-import { theme } from '@styles/'
+import { theme } from '@styles/';
 const red = theme.colors.red;
 const yellow = theme.colors.yellow;
 const green = theme.colors.green;
 
 /** 가용병상률에 따라 컬러를 반환하는 함수 */
-const getErRTavailableBedByColor = (availableBed, totalBed) => {
+const getErRTavailableBedByColor = (
+  availableBed,
+  totalBed,
+  isolation = false,
+) => {
   const availableRate = (availableBed / totalBed) * 100;
-  if (availableRate >= 80) {
-    return green;
-  } else if (availableRate >= 50) {
-    return yellow;
+
+  if (isolation) {
+    if (availableRate === 100) {
+      return green;
+    } else if (availableRate >= 50) {
+      return yellow;
+    } else {
+      return red;
+    }
   } else {
-    return red;
+    if (availableRate >= 80) {
+      return green;
+    } else if (availableRate >= 50) {
+      return yellow;
+    } else {
+      return red;
+    }
   }
 };
 
