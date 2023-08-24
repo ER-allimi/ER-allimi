ee8bec16c25dfeb197ece2d609c14cf408ff8582
hyejin <yhj970102@gmail.com>
2023-08-24 16:15:17 +0900

    [Refactor] EmptyBox 용도 변경

--- er-allimi/src/components/ersBoxes/EmptyBox.jsx
+++ er-allimi/src/components/ersBoxes/EmptyBox.jsx
@@ -1,10 +1,11 @@
+import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import { TbHomeOff } from '@components';
 
-function EmptyBox() {
+function EmptyBox({ height, icon, children }) {
   return (
-    <StyledEmptyBox>
-      <TbHomeOff />이 위치 주변에는 응급실이 없습니다.
+    <StyledEmptyBox height={height}>
+      <Icon>{icon}</Icon>
+      <Content>{children}</Content>
     </StyledEmptyBox>
   );
 }
@@ -15,14 +16,32 @@ const StyledEmptyBox = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100px;
+  height: ${({ height }) => `${height}px`};
   color: ${({ theme }) => theme.colors.gray};
-  font-size: 13px;
+`;
 
+const Icon = styled.span`
   svg {
     margin-bottom: 0.5rem;
     font-size: 1.5rem;
+    color: inherit;
   }
 `;
+
+const Content = styled.div`
+  font-size: 13px;
+  color: inherit;
+`;
+
+EmptyBox.propTypes = {
+  height: PropTypes.number, // 단위: px
+  icon: PropTypes.node,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+};
+
+EmptyBox.defaultProps = {
+  height: 50,
+  children: '내용을 입력하세요.',
+};
 
 export default EmptyBox;
