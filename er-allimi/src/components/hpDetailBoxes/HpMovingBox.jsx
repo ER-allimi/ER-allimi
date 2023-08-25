f8fe78ee39d018b1918a1cbc262abac006d96932
hyejin <yhj970102@gmail.com>
2023-08-25 16:03:38 +0900

    [Refactor] HpMovingBox 내 MovingBox의 prop으로 state넘김

--- er-allimi/src/components/hpDetailBoxes/HpMovingBox.jsx
+++ er-allimi/src/components/hpDetailBoxes/HpMovingBox.jsx
@@ -1,18 +1,33 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 import styled from '@emotion/styled';
 import { MovingBox } from '@components';
+import { showHpMessageState } from '@stores';
 
 function HpMovingBox({ className }) {
+  const [isExpanded, setIsExpanded] = useState(true);
+  const setShowHpMessage = useSetRecoilState(showHpMessageState);
+
+  const handleExpand = () => {
+    isExpanded ? setShowHpMessage(false) : setShowHpMessage(true);
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <StyledMovingErsBox className={className}>
+    <StyledMovingErsBox
+      className={className}
+      isExpanded={isExpanded}
+      handleExpand={handleExpand}
+    >
       하단 디테일 박스
     </StyledMovingErsBox>
   );
 }
 const StyledMovingErsBox = styled(MovingBox)`
-    height: calc((100vh - 70px) / 2);
-  `;
-  
+  height: calc((100vh - 70px) / 2);
+`;
+
 HpMovingBox.propTypes = {
   className: PropTypes.string,
 };
