d95a42a94fc20cc9665dbfb9db2992ec5cd4a1a6
hyejin <yhj970102@gmail.com>
2023-08-25 16:01:31 +0900

    [Refactor] ErsMovingBox 내 MovingBox에 prop으로state넘김

--- er-allimi/src/components/ersBoxes/ErsMovingBox.jsx
+++ er-allimi/src/components/ersBoxes/ErsMovingBox.jsx
@@ -1,14 +1,25 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import styled from '@emotion/styled';
 import { MovingBox, ErsContent } from '@components';
 
 function ErsMovingBox({ className }) {
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   const StyledMovingErsBox = styled(MovingBox)`
     height: calc((100vh - 70px) / 2);
   `;
 
   return (
-    <StyledMovingErsBox className={className}>
+    <StyledMovingErsBox
+      className={className}
+      isExpanded={isExpanded}
+      handleExpand={handleExpand}
+    >
       <ErsContent />
     </StyledMovingErsBox>
   );
