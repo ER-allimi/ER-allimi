6b91b1d5d30d42afc7ba179836896b34a5561c93
hyejin <yhj970102@gmail.com>
2023-09-01 16:04:34 +0900

    [Rename] NotoSans 폰트 파일을 public으로 옮김

--- er-allimi/src/styles/global.js
+++ er-allimi/src/styles/global.js
@@ -48,7 +48,7 @@ const globalStyles = ({ colors, breakPoints }) => css`
 
   @font-face {
     font-family: 'Noto Sans KR';
-    src: url('/src/assets/NotoSansKR-Regular.otf');
+    src: url('/public/assets/NotoSansKR-Regular.otf');
   }
 `;
 
