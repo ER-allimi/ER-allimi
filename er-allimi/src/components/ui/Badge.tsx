039214ebe5b2a2cea83a29b640325668b88cdd9b
hyejin <yhj970102@gmail.com>
2023-10-15 22:40:28 +0900

    [Refactor] Badge 파일 ts 변환

---
+++ er-allimi/src/components/ui/Badge.tsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { theme } from '@styles';
+
+interface BadgeProps {
+  color: keyof typeof theme.colors;
+  outline: boolean;
+  children: string | number | React.ReactNode;
+  className?: string;
+}
+
+Badge.defaultProps = {
+  color: 'gray',
+  outlined: false,
+};
+
+function Badge({ color, outline, children, className, ...rest }: BadgeProps) {
+  return (
+    <StyledBadge
+      color={color}
+      outline={outline}
+      className={className}
+      {...rest}
+    >
+      {children}
+    </StyledBadge>
+  );
+}
+
+interface OutlineProps {
+  theme?: any;
+  outline: boolean;
+  color: string;
+}
+
+const outlined = ({ theme, outline, color }: OutlineProps) =>
+  outline &&
+  css`
+    background-color: white;
+    color: ${theme.colors[color]};
+  `;
+
+const StyledBadge = styled.button`
+  padding: 0.1rem 0.3rem;
+  border: 1px solid ${({ theme, color }) => theme.colors[color]};
+  border-radius: 50%;
+  background-color: ${({ theme, color }) => theme.colors[color]};
+  font-size: 13px;
+  font-weight: 700;
+  word-spacing: -1px;
+  color: white;
+  cursor: pointer;
+
+  ${outlined}
+`;
+
+export default Badge;
