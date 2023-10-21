eee83abc408ba47659872712ea3e29bb8ff05701
ming0830 <minsun0830@naver.com>
2023-10-21 10:06:16 +0900

    [Feat] Box story 생성

---
+++ er-allimi/src/stories/Box.stories.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@components';
+
+const TEXT = '박스 내용';
+export default {
+  title: 'Box',
+  component: Box,
+  args: {
+    children: TEXT,
+  },
+};
+
+export const Default = {};
