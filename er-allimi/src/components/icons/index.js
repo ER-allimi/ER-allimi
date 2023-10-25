fe7e08d19638d49690a9f949d7d18ade5e095696
ming0830 <minsun0830@naver.com>
2023-10-25 18:39:00 +0900

    [Refactor] @react-icons/all-files 경로 수정

--- er-allimi/src/components/icons/index.js
+++ er-allimi/src/components/icons/index.js
@@ -1,51 +1,70 @@
-export {
-  BiCurrentLocation,
-  BiSolidPhone,
-  BiSolidDownArrow,
-  BiSolidUpArrow,
-  BiBody,
-  BiSolidError,
-  BiError,
-  BiLeftArrow,
-  BiRightArrow,
-} from 'react-icons/bi';
-export { GrMapLocation } from 'react-icons/gr';
-export { FaMapMarkedAlt, FaMapMarker, FaBed } from 'react-icons/fa';
-export { RxHamburgerMenu } from 'react-icons/rx';
-export {
-  AiOutlineInfoCircle,
-  AiTwotoneAlert,
-  AiOutlinePlus,
-  AiOutlineMinus,
-  AiFillNotification,
-} from 'react-icons/ai';
-export {
-  IoLocationSharp,
-  IoCloseSharp,
-  IoNotificationsCircleOutline,
-  IoClose,
-} from 'react-icons/io5';
-export { MdOutlineInsertChartOutlined, MdLocationOff } from 'react-icons/md';
-export { VscTriangleDown, VscTriangleUp } from 'react-icons/vsc';
-export {
-  BsFillCircleFill,
-  BsCheckCircleFill,
-  BsFillInfoSquareFill,
-} from 'react-icons/bs';
-export { PiArrowBendUpLeftBold } from 'react-icons/pi';
-export { TbNoteOff, TbHomeOff, TbArticleOff } from 'react-icons/tb';
-export { ImSpinner2 } from 'react-icons/im';
-export {
-  RiCloseCircleFill,
-  RiCloseCircleLine,
-  RiHospitalLine,
-} from 'react-icons/ri';
-export {
-  HiMiniArrowSmallLeft,
-  HiMiniArrowSmallRight,
-  HiMiniArrowLongLeft,
-  HiMiniArrowLongRight,
-} from 'react-icons/hi2';
-export { GiHealingShield } from 'react-icons/gi';
-export { GrAnnounce } from 'react-icons/gr';
-export { FiSearch } from 'react-icons/fi';
+// Bi icons
+export { BiCurrentLocation } from '@react-icons/all-files/bi/BiCurrentLocation';
+export { BiBody } from '@react-icons/all-files/bi/BiBody';
+export { BiError } from '@react-icons/all-files/bi/BiError';
+export { BiLeftArrow } from '@react-icons/all-files/bi/BiLeftArrow';
+export { BiRightArrow } from '@react-icons/all-files/bi/BiRightArrow';
+export { BiNotificationOff } from '@react-icons/all-files/bi/BiNotificationOff';
+
+// Gr icon
+export { GrMapLocation } from '@react-icons/all-files/gr/GrMapLocation';
+export { GrAnnounce } from '@react-icons/all-files/gr/GrAnnounce';
+
+// Fa icons
+export { FaMapMarkedAlt } from '@react-icons/all-files/fa/FaMapMarkedAlt';
+export { FaMapMarker } from '@react-icons/all-files/fa/FaMapMarker';
+export { FaBed } from '@react-icons/all-files/fa/FaBed';
+
+// Gi icon
+export { GiHamburgerMenu } from '@react-icons/all-files/gi/GiHamburgerMenu';
+
+// Ai icons
+export { AiFillWarning } from '@react-icons/all-files/ai/AiFillWarning';
+export { AiOutlineInfoCircle } from '@react-icons/all-files/ai/AiOutlineInfoCircle';
+export { AiTwotoneAlert } from '@react-icons/all-files/ai/AiTwotoneAlert';
+export { AiOutlinePlus } from '@react-icons/all-files/ai/AiOutlinePlus';
+export { AiOutlineMinus } from '@react-icons/all-files/ai/AiOutlineMinus';
+export { AiFillNotification } from '@react-icons/all-files/ai/AiFillNotification';
+
+// Io5 icons
+export { IoLocationSharp } from '@react-icons/all-files/io5/IoLocationSharp';
+export { IoCloseSharp } from '@react-icons/all-files/io5/IoCloseSharp';
+export { IoNotificationsCircleOutline } from '@react-icons/all-files/io5/IoNotificationsCircleOutline';
+export { IoClose } from '@react-icons/all-files/io5/IoClose';
+export { IoReturnDownBackOutline } from '@react-icons/all-files/io5/IoReturnDownBackOutline';
+
+// Hi icons
+export { HiOutlineChartSquareBar } from '@react-icons/all-files/hi/HiOutlineChartSquareBar';
+export { HiCheckCircle } from '@react-icons/all-files/hi/HiCheckCircle';
+
+// Md icons
+export { MdLocationOff } from '@react-icons/all-files/md/MdLocationOff';
+
+// Vsc icons
+export { VscTriangleDown } from '@react-icons/all-files/vsc/VscTriangleDown';
+export { VscTriangleUp } from '@react-icons/all-files/vsc/VscTriangleUp';
+export { VscArrowSmallLeft } from '@react-icons/all-files/vsc/VscArrowSmallLeft';
+export { VscArrowSmallRight } from '@react-icons/all-files/vsc/VscArrowSmallRight';
+
+// Bs icons
+export { BsFillCircleFill } from '@react-icons/all-files/bs/BsFillCircleFill';
+export { BsFillInfoSquareFill } from '@react-icons/all-files/bs/BsFillInfoSquareFill';
+
+// Im icon
+export { ImSpinner2 } from '@react-icons/all-files/im/ImSpinner2';
+
+// Ri icons
+export { RiCloseCircleFill } from '@react-icons/all-files/ri/RiCloseCircleFill';
+export { RiCloseCircleLine } from '@react-icons/all-files/ri/RiCloseCircleLine';
+export { RiHospitalLine } from '@react-icons/all-files/ri/RiHospitalLine';
+export { RiShieldCrossFill } from '@react-icons/all-files/ri/RiShieldCrossFill';
+
+// Fi icon
+export { FiSearch } from '@react-icons/all-files/fi/FiSearch';
+
+// Cg icon
+export { CgPhone } from '@react-icons/all-files/cg/CgPhone';
+
+//Ti icon
+export { TiArrowSortedDown } from '@react-icons/all-files/ti/TiArrowSortedDown';
+export { TiArrowSortedUp } from '@react-icons/all-files/ti/TiArrowSortedUp';
