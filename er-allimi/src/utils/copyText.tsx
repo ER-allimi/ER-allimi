e85c187a8b7c9128847c8e49f18f7b33282b34f3
hyejin <yhj970102@gmail.com>
2023-10-30 16:16:07 +0900

    [Refactor] copyText 파일 ts로 변환

--- er-allimi/src/utils/copyText.tsx
+++ er-allimi/src/utils/copyText.tsx
@@ -1,7 +1,13 @@
 import toast from 'react-hot-toast';
 import { SuccessMessage, ErrorMessage } from '@components';
 
-const copyText = ({ text, successMessage, errorMessage }) => {
+interface CopyTextProps {
+  text: string;
+  successMessage: string;
+  errorMessage: string;
+}
+
+const copyText = ({ text, successMessage, errorMessage }: CopyTextProps) => {
   navigator.clipboard
     .writeText(text)
     .then(() => {
