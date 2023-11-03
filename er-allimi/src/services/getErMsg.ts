600edb7c22e2736b1a2a3554772fdf47cbd5c755
hyejin <yhj970102@gmail.com>
2023-11-03 22:32:28 +0900

    [Refactor] getErList, getErMsg 파일 ts 변환

--- er-allimi/src/services/getErMsg.ts
+++ er-allimi/src/services/getErMsg.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
+
+interface GetErMsgProps {
+  HPID: string;
+}
+
 const serviceKey = encodeURI(import.meta.env.VITE_SERVICE_KEY);
 
-const getErMsg = async ({ HPID }) => {
+const getErMsg = async ({ HPID }: GetErMsgProps) => {
   const url =
     'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEmrrmSrsillDissMsgInqire';
   try {
