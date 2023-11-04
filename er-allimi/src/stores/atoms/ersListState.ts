cee02f0781a2bd82823bdee1a434855ae6dfd02c
hyejin <yhj970102@gmail.com>
2023-11-04 10:37:20 +0900

    [Refactor] ersListState에서 erItemType 지정

--- er-allimi/src/stores/atoms/ersListState.ts
+++ er-allimi/src/stores/atoms/ersListState.ts
@@ -1,7 +1,17 @@
 import { atom } from 'recoil';
 
+interface erItemType {
+  hpid: string;
+  dutyName: string;
+  dutyEmclsName: string;
+  dutyAddr: string;
+  dutyTel3: string;
+  wgs84Lon: number;
+  wgs84Lat: number;
+}
+
 // 전체 응급실 목록
-const ersListState = atom({
+const ersListState = atom<erItemType[]>({
   key: 'ersListState',
   default: [],
 });
