e76d20d44e1f5fd6d8a8788a8ffcec5338ed4a4f
hyejin <yhj970102@gmail.com>
2023-11-07 17:44:52 +0900

    [Refactor] ersRTavailableBed 내 인터페이스를 global로 옮김

--- er-allimi/src/stores/atoms/ersRTavailableBedState.ts
+++ er-allimi/src/stores/atoms/ersRTavailableBedState.ts
@@ -1,9 +1,5 @@
 import { atom } from 'recoil';
 
-interface erRTavailableBedInfoType {
-  [key: string]: any;
-}
-
 // 전체 응급실 실시간 가용 병상 목록
 const ersRTavailableBedState = atom<erRTavailableBedInfoType[]>({
   key: 'ersRTavailableBedState',
