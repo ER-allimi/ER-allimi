c560fe468352c4e5a47d7499ce3205ab04036b8c
hyejin <yhj970102@gmail.com>
2023-11-07 17:44:04 +0900

    [Refactor] ersListState 파일 내 인터페이스를 global로 옮김

--- er-allimi/src/stores/atoms/ersListState.ts
+++ er-allimi/src/stores/atoms/ersListState.ts
@@ -1,15 +1,5 @@
 import { atom } from 'recoil';
 
-interface erItemType {
-  hpid: string;
-  dutyName: string;
-  dutyEmclsName: string;
-  dutyAddr: string;
-  dutyTel3: string;
-  wgs84Lon: number;
-  wgs84Lat: number;
-}
-
 // 전체 응급실 목록
 const ersListState = atom<erItemType[]>({
   key: 'ersListState',
